ee47410385aacbbeca7b280f2c33ba2d8ac720fd
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-06-07 22:18:14 +0300

    CRA + some components Homework from 01/06

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import SiteMapPanel from "./SiteMapPanel/SiteMapPanel";
+
+const Header = (props) => {
+  return (
+    <header>
+      <SiteMapPanel />
+    </header>
+  );
+};
+export default Header;
