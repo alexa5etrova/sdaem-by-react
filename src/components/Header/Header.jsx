37c60f4f3b2b8619923fd8d80928239698cd5713
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-06-10 21:36:33 +0300

    Header NavCatalogue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import Nav from "./Nav/Nav";
 import SiteMapPanel from "./SiteMapPanel/SiteMapPanel";
 
 const Header = (props) => {
   return (
     <header>
       <SiteMapPanel />
+      <Nav />
     </header>
   );
 };
