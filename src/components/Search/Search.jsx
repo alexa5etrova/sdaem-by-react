9506c7bf2f5e94e159b066ab9e2b56590f593852
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-06-28 18:21:00 +0300

    Search bar added

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,37 @@
+import styles from "./Search.module.scss";
+import Input from "./../Input/Input";
+import Button from "../Button/Button";
+import { ReactComponent as SearchIcon } from "./../../assets/icons/search.svg";
+import { useState } from "react";
+
+const Search = (props) => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const changelHandler = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const onSearch = () => {
+    let toFind = searchInput.toLowerCase();
+    props.searchHandler(toFind);
+    setSearchInput("");
+  };
+  return (
+    <div className={styles.searchContainer}>
+      <Input
+        placeholder="Поиск по статьям"
+        type="text"
+        inputStyle="search"
+        value={searchInput}
+        onChange={changelHandler}
+      ></Input>
+      <div className={styles.btnContainer}>
+        <Button className={styles.button} btnStyle="search" onClick={onSearch}>
+          <SearchIcon />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -5,10 +5,12 @@ import styles from "./News.module.scss";
 import { useState } from "react";
 import Pagination from "../../components/Pagination/Pagination";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import Search from "../../components/Search/Search";
 
 const News = (props) => {
   const [firstContentIndex, setFirstContentIndex] = useState();
   const [lastContentIndex, setLastContentIndex] = useState();
+  const [search, setSearch] = useState("");
 
   const getFirstIndex = (index) => {
     setFirstContentIndex(index);
@@ -17,31 +19,39 @@ const News = (props) => {
     setLastContentIndex(index);
   };
 
+  const filteredNewsData = Newsdata.filter(function (item) {
+    if (search === "") {
+      return item;
+    } else {
+      return item.title.toLowerCase().includes(search);
+    }
+  });
+
   return (
     <>
       <nav className={styles.crumbsContainer}>
         <Breadcrumbs crumbs={NewsCrumbs} />
       </nav>
-
+      <Search searchHandler={setSearch}></Search>
       <Htag tag="h1">Новости</Htag>
       <div className={styles.newsContainer}>
-        {Newsdata.slice(firstContentIndex, lastContentIndex).map(function (
-          item
-        ) {
-          return (
-            <NewsCard
-              key={item.id}
-              title={item.title}
-              date={item.date}
-              short={item.short}
-              photo={item.photo}
-            />
-          );
-        })}
+        {filteredNewsData
+          .slice(firstContentIndex, lastContentIndex)
+          .map(function (item) {
+            return (
+              <NewsCard
+                key={item.id}
+                title={item.title}
+                date={item.date}
+                short={item.short}
+                photo={item.photo}
+              />
+            );
+          })}
       </div>
       <div className={styles.paginationContainer}>
         <Pagination
-          data={Newsdata}
+          data={filteredNewsData}
           sendFirstIndex={getFirstIndex}
           sendLastIndex={getLastIndex}
         />
