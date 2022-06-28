958ec50e4c3d026b4d984a3b0ece5233584cbdef
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-06-29 00:12:11 +0300

    NewsItem Page logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import "./App.css";
 import News from "./pages/News/News";
 import NotFound from "./pages/NotFound/NotFound";
 import Layout from "./Layout/Layout";
+import NewsItem from "./pages/NewsItem/NewsItem";
+import { NewsCrumbs } from "./data/Newsdata";
 
 function App() {
+  // let { newsId } = useParams();
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<News />} />
+
+          <Route
+            path="/news/131"
+            element={<NewsItem crumbs={NewsCrumbs} id={131} />}
+          />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/NewsItem/NewsItem.jsx
+++ src/pages/NewsItem/NewsItem.jsx
@@ -1,16 +1,54 @@
 import styles from "./NewsItem.module.scss";
 import Breadcrumbs from "./../../components/Breadcrumbs/Breadcrumbs";
 import Htag from "./../../components/Htag/Htag";
+import DateTag from "../../components/DateTag/DateTag";
+import SocialMediaLogo from "./../../components/SocialMediaLogo/SocialMediaLogo";
+import photo from "./../../assets/images/photoNews.jpg";
+import { Newsdata } from "../../data/Newsdata";
+import NewsCard from "../../components/NewsCard/NewsCard";
 
 const NewsItem = (props) => {
-  let crumbs = [...props.crumbs, props.title];
+  let showenNews = Newsdata.find((item) => item.id === props.id);
+  let crumbs = [
+    ...props.crumbs,
+    { title: showenNews.title, url: `/news/${showenNews.id}`, id: 202 },
+  ];
+
+  let index = Newsdata.findIndex((item) => item.id === props.id);
+
+  let readMore = [...Newsdata].splice(index, 3);
+  console.log(readMore);
+
   return (
     <>
       <div className={styles.newsItem}>
-        <nav className={styles.crumbsContainer}>
-          <Breadcrumbs crumbs={crumbs} />
-        </nav>
-        <Htag tag="h1">{props.title}</Htag>
+        <div className={styles.newsHeader}>
+          <nav className={styles.crumbsContainer}>
+            <Breadcrumbs crumbs={crumbs} />
+          </nav>
+          <Htag tag="h1">{showenNews.title}</Htag>
+          <div className={styles.tags}>
+            <DateTag dateStyle="violet" date={showenNews.date} />
+            <div>
+              Поделиться: <SocialMediaLogo />
+            </div>
+          </div>
+        </div>
+        <div className={styles.photo}>
+          <img src={photo} alt={showenNews.title} />
+        </div>
+        <div className={styles.newsText}>{showenNews.short}</div>
+      </div>
+      <div className={styles.readMore}>
+        {readMore.map((item) => (
+          <NewsCard
+            key={item.id}
+            title={item.title}
+            date={item.date}
+            short={item.short}
+            photo={item.photo}
+          />
+        ))}
       </div>
     </>
   );
