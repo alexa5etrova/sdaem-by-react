1c49574895c3b35c5bc7538a50c9bbea4a0fbe24
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-07-25 13:27:22 +0300

    PaymentsLogo upd

--- src/components/PaymentsLogo/PaymentsLogo.jsx
+++ src/components/PaymentsLogo/PaymentsLogo.jsx
@@ -1,33 +1,16 @@
-import belkart from "./../../assets/images/belkart.png";
-import mastercard from "./../../assets/images/mastercard.png";
-import securecode from "./../../assets/images/securecode.png";
-import verifiedByVisa from "./../../assets/images/verifiedByVisa.png";
-import visa from "./../../assets/images/visa.png";
-import webpay from "./../../assets/images/webpay.png";
+import { PAYMENTS_METHOD } from "./../../data/const";
+
 import styles from "./PaymentsLogo.module.scss";
 
 const PaymentsLogo = () => {
   return (
     <div>
       <ul className={styles.paymentsList}>
-        <li className={styles.paymentsListItem}>
-          <img src={visa} alt="visa logo" />
-        </li>
-        <li className={styles.paymentsListItem}>
-          <img src={webpay} alt="webpay logo" />
-        </li>
-        <li className={styles.paymentsListItem}>
-          <img src={verifiedByVisa} alt="verified By Visa logo" />
-        </li>
-        <li className={styles.paymentsListItem}>
-          <img src={mastercard} alt="mastercard" />
-        </li>
-        <li className={styles.paymentsListItem}>
-          <img src={securecode} alt="securecode" />
-        </li>
-        <li className={styles.paymentsListItem}>
-          <img src={belkart} alt="belcard" />
-        </li>
+        {PAYMENTS_METHOD.map((pay) => (
+          <li className={styles.paymentsListItem} key={pay.id}>
+            <img src={pay.src} alt={pay.src} />
+          </li>
+        ))}
       </ul>
     </div>
   );
