45f0bfe53672a4f3943ef633b73084ffa96fca26
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-07-25 22:39:28 +0300

    Payment_method upd

--- src/data/contacts.jsx
+++ src/data/contacts.jsx
@@ -7,15 +7,15 @@ export const MESSAGE_SENT_FAILED = {
   text: "",
 };
 
-export const PAYMENTS_METHOD = [
-  { src: "./images/visa.png", name: "Visa", id: 1 },
-  { src: "./images/webpay.png", name: "Webpay", id: 2 },
-  { src: "./images/verifiedByVisa.png", name: "Verified By Visa", id: 3 },
-  { src: "./images/mastercard.png", name: "Master Card", id: 4 },
-  { src: "./images/securecode.png", name: "Secure code", id: 5 },
-  { src: "./images/verifiedByVisa.png", name: "Verified By Visa", id: 6 },
-  { src: "./images/belkart.png", name: "Belkart", id: 7 },
-];
+// export const PAYMENTS_METHOD = [
+//   { src: "./assets/images/visa.png", name: "Visa", id: 1 },
+//   { src: "./images/webpay.png", name: "Webpay", id: 2 },
+//   { src: "./images/verifiedByVisa.png", name: "Verified By Visa", id: 3 },
+//   { src: "./images/mastercard.png", name: "Master Card", id: 4 },
+//   { src: "./images/securecode.png", name: "Secure code", id: 5 },
+//   { src: "./images/verifiedByVisa.png", name: "Verified By Visa", id: 6 },
+//   { src: "./images/belkart.png", name: "Belkart", id: 7 },
+// ];
 
 export const COMPANY = {
   name: "ИП Шушкевич Андрей Викторович ",
