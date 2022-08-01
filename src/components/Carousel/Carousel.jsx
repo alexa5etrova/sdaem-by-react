208b0ff0220b176a5f546a97072feb96d8d76332
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-08-01 14:27:17 +0300

    Swiper completed

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,12 +1,21 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
 import FlatCard from "../FlatCard/FlatCard";
+import { SwiperButton } from "./SwiperButton/SwiperButton";
 
 const Carousel = ({ flats }) => {
   return (
-    <>
-      {flats.slice(0, 3).map((item) => (
-        <FlatCard key={item.id} flat={item} />
+    <Swiper slidesPerView={3} spaceBetween={30} loop={true}>
+      {flats.map((item) => (
+        <SwiperSlide key={item.id}>
+          <FlatCard flat={item} />
+        </SwiperSlide>
       ))}
-    </>
+      <SwiperButton />
+    </Swiper>
   );
 };
 
