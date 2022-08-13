f5031b386aa7dade6caf44944c14e20b41bdcd51
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-08-13 12:58:34 +0300

    NewsSlice upd

--- src/redux/newsSlice.js
+++ src/redux/newsSlice.js
@@ -14,24 +14,7 @@ const newsSlice = createSlice({
     status: null,
     error: null,
   },
-  reducers: {
-    // addTodo(state, action) {
-    //   state.todos.push({
-    //     id: new Date().toISOString(),
-    //     text: action.payload.text,
-    //     completed: false,
-    //   });
-    // },
-    // toggleComplete(state, action) {
-    //   const toggledTodo = state.todos.find(
-    //     (todo) => todo.id === action.payload.id
-    //   );
-    //   toggledTodo.completed = !toggledTodo.completed;
-    // },
-    // removeTodo(state, action) {
-    //   state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
-    // },
-  },
+  reducers: {},
   extraReducers: {
     [fetchNews.pending]: (state) => {
       state.status = "loading";
@@ -47,7 +30,5 @@ const newsSlice = createSlice({
     },
   },
 });
-
-export const { addTodo, toggleComplete, removeTodo } = newsSlice.actions;
 
 export default newsSlice.reducer;
